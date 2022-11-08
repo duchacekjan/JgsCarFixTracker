f9b09bee0ca0fef8ce1fd0d2ffd55e82e91abba7
jgs <jgs.nuget@gmail.com>
2022-11-08 10:52:52 +0100

    Adding car detail. Moved some styles to styles.css

--- src/app/components/cars/car-detail/car-detail.component.ts
+++ src/app/components/cars/car-detail/car-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import {Component, OnInit} from '@angular/core';
+import {Car} from "../../../models/car";
+import {ActivatedRoute, Router} from "@angular/router";
+import {CarsService} from "../../../services/cars.service";
 
 @Component({
   selector: 'app-car-detail',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarDetailComponent implements OnInit {
 
-  constructor() { }
+  car: Car = {
+    brand: '',
+    model: '',
+    fixes: []
+  };
+  isNew: boolean = false;
+  private carKey: string | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private carsService: CarsService,
+    private location: Location,
+    private router: Router
+  ) {  }
 
   ngOnInit(): void {
+    this.getCar();
   }
 
+  getCar(): void {
+    const id = this.carKey ? this.carKey : String(this.route.snapshot.paramMap.get('id'));
+    this.isNew = id === 'new'
+    if (this.isNew) {
+      this.car = {
+        brand: '',
+        model: '',
+        fixes: []
+      };
+    } else {
+      this.carsService.getCar(id)
+        .subscribe(data => {
+          this.car = data;
+          console.log(`Car: ${this.car?.key}`)
+          if (!this.car.fixes) {
+            this.car.fixes = [];
+          }
+          this.carKey = data.key ? data.key : null;
+        });
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(car: Car): void {
+    console.log('Save called');
+    this.carsService.upsert(car)
+      .then(key => {
+        if (this.carKey === key) {
+          this.getCar();
+          window.alert('Saved');
+        } else {
+          this.carKey = key;
+          this.router.navigate([`/cars/detail/${this.carKey}`], {replaceUrl: true})
+            .then(() => {
+              this.getCar();
+            });
+        }
+      })
+      .catch(err => console.log(err));
+  }
 }
--- src/app/components/cars/cars.module.ts
+++ src/app/components/cars/cars.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CarsRoutingModule } from './cars-routing.module';
 import { CarListComponent } from './car-list/car-list.component';
 import { CarDetailComponent } from './car-detail/car-detail.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    CarsRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        CarsRoutingModule,
+        FormsModule
+    ],
   declarations: [CarListComponent, CarDetailComponent]
 })
 export class CarsModule { }
