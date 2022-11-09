b7758a2f9b3fcdeb19f4ba67efdb6c570b972324
jgs <jgs.nuget@gmail.com>
2022-11-09 11:28:59 +0100

    Locale. Tries with sign-in page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { browserLocalPersistence, getAuth, provideAuth, setPersistence, signInWithEmailAndPassword } from '@angular/fire/auth';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
-import { RouterModule } from '@angular/router';
-import { environment } from '../environments/environment';
-import { AppComponent } from './app.component';
-import { LayoutModule } from './layout/layout.module';
-import { AuthService } from './services/auth.service';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {browserLocalPersistence, getAuth, provideAuth, setPersistence, signInWithEmailAndPassword} from '@angular/fire/auth';
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/compat/database';
+import {getDatabase, provideDatabase} from '@angular/fire/database';
+import {RouterModule} from '@angular/router';
+import {environment} from '../environments/environment';
+import {AppComponent} from './app.component';
+import {LayoutModule} from './layout/layout.module';
+import {AuthService} from './services/auth.service';
+import {registerLocaleData} from "@angular/common";
+import localeCz from '@angular/common/locales/cs';
+
+registerLocaleData(localeCz);
 
 @NgModule({
   declarations: [
@@ -27,7 +31,8 @@ import { AuthService } from './services/auth.service';
     RouterModule.forRoot([]),
     LayoutModule
   ],
-  providers: [AuthService],
+  providers: [AuthService, {provide: LOCALE_ID, useValue: 'cs-CZ'}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
