61234d2d73d89dbbe16adb9c778d1e9782e1f861
jgs <jgs.nuget@gmail.com>
2023-02-22 08:48:26 +0100

    Prepared vertical strech. For now is using offset.

--- src/app/modules/cars/edit-table/edit-table.component.ts
+++ src/app/modules/cars/edit-table/edit-table.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, HostBinding, Input, OnDestroy, OnInit, Output, SimpleChanges, ViewChild} from '@angular/core';
 import {MatPaginator} from "@angular/material/paginator";
 import {Subscription} from "rxjs";
 import {MatTableDataSource} from "@angular/material/table";
@@ -13,6 +13,7 @@ import {MatSort} from "@angular/material/sort";
 export class EditTableComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @Input() table_config = new TableConfig([]);
+  @Input() offset: string = "0";
 
   @Output() onRowAdd = new EventEmitter<any>();
   @Output() onRowEdit = new EventEmitter<any>();
@@ -25,10 +26,15 @@ export class EditTableComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
+  @HostBinding("style.--offset") hostOffset: string = "0";
   // Subscriptions
   private data_change_sub!: Subscription;
 
   constructor() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.hostOffset = this.offset;
   }
 
   ngOnInit(): void {
