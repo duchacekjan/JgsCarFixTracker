ba5b0958dfef33d73ecb678b8533aebebcd4c66c
jgs <jgs.nuget@gmail.com>
2023-04-06 07:23:19 +0200

    Created notifications table

---
+++ src/app/modules/notifications/notifications-table/notifications-table.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-table',
+  templateUrl: './notifications-table.component.html',
+  styleUrls: ['./notifications-table.component.scss']
+})
+export class NotificationsTableComponent {
+
+}
