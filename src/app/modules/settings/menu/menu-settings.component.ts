cc83dd0fa4fdc9c80002644be3dbceac5f8f443e
jgs <jgs.nuget@gmail.com>
2023-04-12 09:36:07 +0200

    Prepared drawer

--- src/app/modules/settings/menu/menu-settings.component.ts
+++ src/app/modules/settings/menu/menu-settings.component.ts
@@ -6,6 +6,7 @@ import {MenuService} from "../../../services/menu.service";
 import {Observable} from "rxjs";
 import {MenuItem} from "../../../models/menuItem";
 import {Action} from "../../../models/action";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-menu-settings',
@@ -13,7 +14,11 @@ import {Action} from "../../../models/action";
   styleUrls: ['./menu-settings.component.scss']
 })
 export class MenuSettingsComponent extends AfterNavigatedHandler {
-  menuItems: Observable<MenuItem[]>
+  menuItems: Observable<MenuItem[]>;
+  isDrawerOpened: boolean = false;
+  isNewRowBeingAdded: boolean = false;
+
+  menuItemForm!: FormGroup;
 
   constructor(
     private readonly menuService: MenuService,
@@ -21,6 +26,14 @@ export class MenuSettingsComponent extends AfterNavigatedHandler {
     navigation: NavigationService) {
     super(route, navigation);
     this.menuItems = this.menuService.getAllItems();
+    this.menuItemForm = new FormGroup({
+      key: new FormControl(undefined),
+      icon: new FormControl(''),
+      route: new FormControl(''),
+      tooltip: new FormControl(''),
+      name: new FormControl(''),
+      allowed: new FormControl([]),
+    });
   }
 
   protected override backLinkIfNotPresent = '/';
@@ -37,7 +50,11 @@ export class MenuSettingsComponent extends AfterNavigatedHandler {
   }
 
   select(item: MenuItem) {
+    this.isDrawerOpened = true;
+  }
 
+  updateTableData() {
+    this.isDrawerOpened = false;
   }
 
   private callAdd() {
