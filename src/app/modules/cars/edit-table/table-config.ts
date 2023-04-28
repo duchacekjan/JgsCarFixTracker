c8f49d619e3560b53a907c5c30843359525711ba
Jan Ducháček <jgs.nuget@gmail.com>
2023-04-28 16:49:10 +0200

    Issues/74 add date column (#75)
    * Added support for hiding columns. Added date column for fixes on non mobile screens
    
    * Updated version

--- src/app/modules/cars/edit-table/table-config.ts
+++ src/app/modules/cars/edit-table/table-config.ts
@@ -1,10 +1,20 @@
 import {Subject} from "rxjs";
 
 export class TableConfig {
-  constructor(columns: string[]) {
+  constructor(columns: ColumnConfig[]) {
     this.columns = columns;
   }
 
-  columns: string[]
+  columns: ColumnConfig[]
   table_data_changer = new Subject<any>()
+}
+
+export class ColumnConfig {
+  constructor(columnName: string, hideOnXSmall: boolean = false) {
+    this.columnName = columnName;
+    this.hideOnXSmall = hideOnXSmall;
+  }
+
+  columnName: string
+  hideOnXSmall: boolean
 }
